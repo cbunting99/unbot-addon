8af7c9ea72bbb73be385d27f5e60f572d706f0f2
Chris Bunting <cbuntingde@gmail.com>
2023-10-18 10:35:42 -0400

    Translation

--- UnBot/ExecuteCommand.lua
+++ UnBot/ExecuteCommand.lua
@@ -32,19 +32,19 @@ function CommandShowStrategyFrame(index)
 	local isParty = UnitInParty("target");
 	local isRaid = UnitInRaid("target");
 	if (targetName == nil or targetName == "") then
-		DisplayInfomation("你当前没有选择目标。");
+		DisplayInfomation("You currently have no target selected。");
 		return;
 	end
 	if (not UnitIsPlayer("target")) then
-		DisplayInfomation("需要选择一个玩家机器人作为目标。");
+		DisplayInfomation("Need to select a player bot as target。");
 		return;
 	end
 	if (isParty == nil and isRaid == nil) then
-		DisplayInfomation("选择目标不在你的队伍中。");
+		DisplayInfomation("Select a target that is not in your team。");
 		return;
 	end
 	-- if (not IsRealPartyLeader()) then
-	-- 	DisplayInfomation("你当前不是队伍领袖。");
+	-- 	DisplayInfomation("You are not currently the team leader。");
 	-- 	return;
 	-- end
 
@@ -61,7 +61,7 @@ function CommandFriendsOnline(index)
 			end
 		end
 	else
-		DisplayInfomation("你当前没有好友。");
+		DisplayInfomation("You currently have no friends。");
 	end
 end
 
@@ -84,14 +84,14 @@ function CommandFriendsInvite(index)
 			end
 		end
 	else
-		DisplayInfomation("你当前没有好友。");
+		DisplayInfomation("You currently have no friends。");
 	end
 end
 
 function CommandListIcons(index)
 	local iconsList = {};
 	for i=1, #UnBotCommandIconsPath do iconsList[i] = {[1]=i} end
-	CreateIconsByUnBotBagsFrame(0, "UnBotBagsFrameIcon",0,false,iconsList,UnitName("player"),UnitClass("player")," 所有图标展示",nil,nil,GetIconFunc);
+	CreateIconsByUnBotBagsFrame(0, "UnBotBagsFrameIcon",0,false,iconsList,UnitName("player"),UnitClass("player")," All icons displayed",nil,nil,GetIconFunc);
 end
 
 function CommandInspectFrame(index)
@@ -100,19 +100,19 @@ function CommandInspectFrame(index)
 	local isParty = UnitInParty("target");
 	local isRaid = UnitInRaid("target");
 	if (targetName == nil or targetName == "") then
-		DisplayInfomation("你当前没有选择目标。");
+		DisplayInfomation("You currently have no target selected。");
 		return;
 	end
 	if (not UnitIsPlayer("target")) then
-		DisplayInfomation("需要选择一个玩家机器人作为目标。");
+		DisplayInfomation("Need to select a player bot as target。");
 		return;
 	end
 	if (isParty == nil and isRaid == nil) then
-		DisplayInfomation("选择目标不在你的队伍中。");
+		DisplayInfomation("Select a target that is not in your team。");
 		return;
 	end
 	-- if (not IsRealPartyLeader()) then
-	-- 	DisplayInfomation("你当前不是队伍领袖。");
+	-- 	DisplayInfomation("You are not currently the team leader。");
 	-- 	return;
 	-- end
 	InspectFrame_Show("target");
@@ -121,15 +121,15 @@ end
 function CommandInitForLevel(index)
 	local targetName = UnitName("target");
 	if (targetName == nil or targetName == "") then
-		DisplayInfomation("你当前没有选择目标。");
+		DisplayInfomation("You currently have no target selected。");
 		return;
 	end
 	if (not UnitIsPlayer("target")) then
-		DisplayInfomation("需要选择一个玩家机器人作为目标。");
+		DisplayInfomation("Need to select a player bot as target。");
 		return;
 	end
 	-- if (not IsRealPartyLeader()) then
-	-- 	DisplayInfomation("你当前不是队伍领袖。");
+	-- 	DisplayInfomation("You are not currently the team leader。");
 	-- 	return;
 	-- end
 	local lv = UnitLevel("player");
@@ -138,7 +138,7 @@ end
 
 function CommandReadyCheck(index)
 	-- if (not IsRealPartyLeader()) then
-	-- 	DisplayInfomation("你当前不是队伍领袖。");
+	-- 	DisplayInfomation("You are not currently the team leader。");
 	-- 	return;
 	-- end
 	DoReadyCheck();
@@ -146,7 +146,7 @@ end
 
 function CommandCombatStop(index)
 	-- if (not IsRealPartyLeader()) then
-	-- 	DisplayInfomation("你当前不是队伍领袖。");
+	-- 	DisplayInfomation("You are not currently the team leader。");
 	-- 	return;
 	-- end
 
@@ -155,7 +155,7 @@ function CommandCombatStop(index)
 			for i=1, GetNumRaidMembers(), 1 do
 				local name = (UnitName("raid"..i));
 				if (name) then
-					DisplayInfomation("团队中的 "..name.." 脱离战斗");
+					DisplayInfomation("in the team "..name.." out of combat");
 					SendChatMessage(UnBotExecuteCommand[index]..name, "PARTY");
 				end
 			end
@@ -166,7 +166,7 @@ function CommandCombatStop(index)
 			for i=1, GetNumPartyMembers(), 1 do
 				local name = (UnitName("party"..i));
 				if (name) then
-					DisplayInfomation("小队中的 "..name.." 脱离战斗");
+					DisplayInfomation("in the squad "..name.." out of combat");
 					SendChatMessage(UnBotExecuteCommand[index]..name, "PARTY");
 				end
 			end
@@ -176,7 +176,7 @@ end
 
 function CommandUninvite(index)
 	-- if (not UnitIsPartyLeader("player")) then
-	-- 	DisplayInfomation("你当前不是队伍领袖。");
+	-- 	DisplayInfomation("You are not currently the team leader。");
 	-- 	return;
 	-- end
 	SendChatMessage("nc +grind", "PARTY");
@@ -208,22 +208,22 @@ function CommandUnBotItemList(index)
 	local isParty = UnitInParty("target");
 	local isRaid = UnitInRaid("target");
 	if (targetName == nil or targetName == "") then
-		DisplayInfomation("你当前没有选择目标。");
+		DisplayInfomation("You currently have no target selected。");
 		return;
 	end
 	if (not UnitIsPlayer("target")) then
-		DisplayInfomation("需要选择一个玩家机器人作为目标。");
+		DisplayInfomation("Need to select a player bot as target。");
 		return;
 	end
 	if (isParty == nil and isRaid == nil) then
-		DisplayInfomation("选择目标不在你的队伍中。");
+		DisplayInfomation("Select a target that is not in your team。");
 		return;
 	end
 	-- if (not IsRealPartyLeader()) then
-	-- 	DisplayInfomation("你当前不是队伍领袖。");
+	-- 	DisplayInfomation("You are not currently the team leader。");
 	-- 	return;
 	-- end
-	CreateIconsByUnBotBagsFrame(1, "UnBotItemList"..targetName,1,false,{},targetName,targetClass," 查看道具",FlushItemsToBags,nil,GetItemFunc);
+	CreateIconsByUnBotBagsFrame(1, "UnBotItemList"..targetName,1,false,{},targetName,targetClass," View props",FlushItemsToBags,nil,GetItemFunc);
 end
 
 function CommandUnBotDestroyItem(index)
@@ -232,22 +232,22 @@ function CommandUnBotDestroyItem(index)
 	local isParty = UnitInParty("target");
 	local isRaid = UnitInRaid("target");
 	if (targetName == nil or targetName == "") then
-		DisplayInfomation("你当前没有选择目标。");
+		DisplayInfomation("You currently have no target selected。");
 		return;
 	end
 	if (not UnitIsPlayer("target")) then
-		DisplayInfomation("需要选择一个玩家机器人作为目标。");
+		DisplayInfomation("Need to select a player bot as target。");
 		return;
 	end
 	if (isParty == nil and isRaid == nil) then
-		DisplayInfomation("选择目标不在你的队伍中。");
+		DisplayInfomation("Select a target that is not in your team。");
 		return;
 	end
 	-- if (not IsRealPartyLeader()) then
-	-- 	DisplayInfomation("你当前不是队伍领袖。");
+	-- 	DisplayInfomation("You are not currently the team leader。");
 	-- 	return;
 	-- end
-	CreateIconsByUnBotBagsFrame(3, "UnBotDestroyItem"..targetName,1,true,{},targetName,targetClass," 丢弃道具",FlushItemsToBags,UnBotExecuteCommand[index],GetItemFunc);
+	CreateIconsByUnBotBagsFrame(3, "UnBotDestroyItem"..targetName,1,true,{},targetName,targetClass," discard props",FlushItemsToBags,UnBotExecuteCommand[index],GetItemFunc);
 end
 
 function CommandUnBotEquipItem(index)
@@ -256,22 +256,22 @@ function CommandUnBotEquipItem(index)
 	local isParty = UnitInParty("target");
 	local isRaid = UnitInRaid("target");
 	if (targetName == nil or targetName == "") then
-		DisplayInfomation("你当前没有选择目标。");
+		DisplayInfomation("You currently have no target selected。");
 		return;
 	end
 	if (not UnitIsPlayer("target")) then
-		DisplayInfomation("需要选择一个玩家机器人作为目标。");
+		DisplayInfomation("Need to select a player bot as target。");
 		return;
 	end
 	if (isParty == nil and isRaid == nil) then
-		DisplayInfomation("选择目标不在你的队伍中。");
+		DisplayInfomation("Select a target that is not in your team。");
 		return;
 	end
 	-- if (not IsRealPartyLeader()) then
-	-- 	DisplayInfomation("你当前不是队伍领袖。");
+	-- 	DisplayInfomation("You are not currently the team leader。");
 	-- 	return;
 	-- end
-	CreateIconsByUnBotBagsFrame(2, "UnBotEquipItem"..targetName,1,true,{},targetName,targetClass," 装备道具",FlushItemsToBags,UnBotExecuteCommand[index],GetItemFunc);
+	CreateIconsByUnBotBagsFrame(2, "UnBotEquipItem"..targetName,1,true,{},targetName,targetClass," Equipment props",FlushItemsToBags,UnBotExecuteCommand[index],GetItemFunc);
 end
 
 function CommandUnBotSellItem(index)
@@ -280,22 +280,22 @@ function CommandUnBotSellItem(index)
 	local isParty = UnitInParty("target");
 	local isRaid = UnitInRaid("target");
 	if (targetName == nil or targetName == "") then
-		DisplayInfomation("你当前没有选择目标。");
+		DisplayInfomation("You currently have no target selected。");
 		return;
 	end
 	if (not UnitIsPlayer("target")) then
-		DisplayInfomation("需要选择一个玩家机器人作为目标。");
+		DisplayInfomation("Need to select a player bot as target。");
 		return;
 	end
 	if (isParty == nil and isRaid == nil) then
-		DisplayInfomation("选择目标不在你的队伍中。");
+		DisplayInfomation("Select a target that is not in your team。");
 		return;
 	end
 	-- if (not IsRealPartyLeader()) then
-	-- 	DisplayInfomation("你当前不是队伍领袖。");
+	-- 	DisplayInfomation("You are not currently the team leader。");
 	-- 	return;
 	-- end
-	CreateIconsByUnBotBagsFrame(4, "UnBotSellItem"..targetName,1,true,{},targetName,targetClass," 卖出道具",FlushItemsToBags,UnBotExecuteCommand[index],GetItemFunc);
+	CreateIconsByUnBotBagsFrame(4, "UnBotSellItem"..targetName,1,true,{},targetName,targetClass," Sell props",FlushItemsToBags,UnBotExecuteCommand[index],GetItemFunc);
 end
 
 function CommandUnBotUseItem(index)
@@ -304,22 +304,22 @@ function CommandUnBotUseItem(index)
 	local isParty = UnitInParty("target");
 	local isRaid = UnitInRaid("target");
 	if (targetName == nil or targetName == "") then
-		DisplayInfomation("你当前没有选择目标。");
+		DisplayInfomation("You currently have no target selected。");
 		return;
 	end
 	if (not UnitIsPlayer("target")) then
-		DisplayInfomation("需要选择一个玩家机器人作为目标。");
+		DisplayInfomation("Need to select a player bot as target。");
 		return;
 	end
 	if (isParty == nil and isRaid == nil) then
-		DisplayInfomation("选择目标不在你的队伍中。");
+		DisplayInfomation("Select a target that is not in your team。");
 		return;
 	end
 	-- if (not IsRealPartyLeader()) then
-	-- 	DisplayInfomation("你当前不是队伍领袖。");
+	-- 	DisplayInfomation("You are not currently the team leader。");
 	-- 	return;
 	-- end
-	CreateIconsByUnBotBagsFrame(5, "UnBotUseItem"..targetName,1,true,{},targetName,targetClass," 使用道具",FlushItemsToBags,UnBotExecuteCommand[index],GetItemFunc);
+	CreateIconsByUnBotBagsFrame(5, "UnBotUseItem"..targetName,1,true,{},targetName,targetClass," Use props",FlushItemsToBags,UnBotExecuteCommand[index],GetItemFunc);
 end
 
 function CommandUnBotSpells(index)
@@ -328,22 +328,22 @@ function CommandUnBotSpells(index)
 	local isParty = UnitInParty("target");
 	local isRaid = UnitInRaid("target");
 	if (targetName == nil or targetName == "") then
-		DisplayInfomation("你当前没有选择目标。");
+		DisplayInfomation("You currently have no target selected。");
 		return;
 	end
 	if (not UnitIsPlayer("target")) then
-		DisplayInfomation("需要选择一个玩家机器人作为目标。");
+		DisplayInfomation("Need to select a player bot as target。");
 		return;
 	end
 	if (isParty == nil and isRaid == nil) then
-		DisplayInfomation("选择目标不在你的队伍中。");
+		DisplayInfomation("Select a target that is not in your team。");
 		return;
 	end
 	-- if (not IsRealPartyLeader()) then
-	-- 	DisplayInfomation("你当前不是队伍领袖。");
+	-- 	DisplayInfomation("You are not currently the team leader。");
 	-- 	return;
 	-- end
-	CreateIconsByUnBotBagsFrame(0, "UnBotSpells"..targetName,2,false,{},targetName,targetClass," 施放技能",FlushItemsToBags,UnBotExecuteCommand[index],GetItemFunc);
+	CreateIconsByUnBotBagsFrame(0, "UnBotSpells"..targetName,2,false,{},targetName,targetClass," cast skill",FlushItemsToBags,UnBotExecuteCommand[index],GetItemFunc);
 end
 
 function CommandAddFriends(index)
