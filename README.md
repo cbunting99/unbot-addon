10eab1c8b07ca603e808aeacb6b06719ae51c578
Chris Bunting <cbuntingde@gmail.com>
2023-10-23 05:54:43 -0400

    update - fixes

--- README.md
+++ README.md
@@ -2,10 +2,9 @@
 
 ///////////////////////////////////////
 
-I am trying to translate this addon to English for a small project, however, I dont speak or read chinese.
+I am working on translating this addon to English for a small project, however, I dont speak or read chinese.
 
 //////////////////////////////////////
-
 
 A robot control plug-in modified based on the UnBot plug-in. As a supporting plug-in for the AzerothCore robot module, it is convenient for players to control robots.
 
@@ -13,13 +12,17 @@ Currently, it only has good support for Chinese clients.
 
 Installation method: Place UnBot and YssBossLoot in the Interface/AddOns/ directory
 
+The control / menu bar
+
+![](docs/bar.jpg)
+
 Plug-in interface display:
 
-![](docs/display.png)
+![](docs/display.jpg)
 
 Add a specified professional robot:
 
-![](docs/addclass.png)
+![](docs/addclass.jpg)
 
 
 
--- UnBot/Bindings.lua
+++ UnBot/Bindings.lua
@@ -30,15 +30,15 @@ elseif (GetLocale() == "zhTW") then
 	_G["BINDING_NAME_CLICK UnBotCommandButtonFunc:LeftButton"] = "机器人窗口关闭快捷鍵";
 else
 	BINDING_HEADER_UnBotFrame = "|cff0000ffUnBot Controller Bar|r";
-	_G["BINDING_NAME_CLICK UnBotCommandButton1:LeftButton"] = "Un Bot Command ActionButton 1";
-	_G["BINDING_NAME_CLICK UnBotCommandButton2:LeftButton"] = "Un Bot Command ActionButton 2";
-	_G["BINDING_NAME_CLICK UnBotCommandButton3:LeftButton"] = "Un Bot Command ActionButton 3";
-	_G["BINDING_NAME_CLICK UnBotCommandButton4:LeftButton"] = "Un Bot Command ActionButton 4";
-	_G["BINDING_NAME_CLICK UnBotCommandButton5:LeftButton"] = "Un Bot Command ActionButton 5";
-	_G["BINDING_NAME_CLICK UnBotCommandButton6:LeftButton"] = "Un Bot Command ActionButton 6";
-	_G["BINDING_NAME_CLICK UnBotCommandButton7:LeftButton"] = "Un Bot Command ActionButton 7";
-	_G["BINDING_NAME_CLICK UnBotCommandButton8:LeftButton"] = "Un Bot Command ActionButton 8";
-	_G["BINDING_NAME_CLICK UnBotCommandButton9:LeftButton"] = "Un Bot Command ActionButton 9";
-	_G["BINDING_NAME_CLICK UnBotCommandButton10:LeftButton"] = "Un Bot Command ActionButton 10";
-	_G["BINDING_NAME_CLICK UnBotCommandButtonFunc:LeftButton"] = "Un Bot Close Frame ActionButton";
+	_G["BINDING_NAME_CLICK UnBotCommandButton1:LeftButton"] = "Unbot Command ActionButton 1";
+	_G["BINDING_NAME_CLICK UnBotCommandButton2:LeftButton"] = "Unbot Command ActionButton 2";
+	_G["BINDING_NAME_CLICK UnBotCommandButton3:LeftButton"] = "Unbot Command ActionButton 3";
+	_G["BINDING_NAME_CLICK UnBotCommandButton4:LeftButton"] = "Unbot Command ActionButton 4";
+	_G["BINDING_NAME_CLICK UnBotCommandButton5:LeftButton"] = "Unbot Command ActionButton 5";
+	_G["BINDING_NAME_CLICK UnBotCommandButton6:LeftButton"] = "Unbot Command ActionButton 6";
+	_G["BINDING_NAME_CLICK UnBotCommandButton7:LeftButton"] = "Unbot Command ActionButton 7";
+	_G["BINDING_NAME_CLICK UnBotCommandButton8:LeftButton"] = "Unbot Command ActionButton 8";
+	_G["BINDING_NAME_CLICK UnBotCommandButton9:LeftButton"] = "Unbot Command ActionButton 9";
+	_G["BINDING_NAME_CLICK UnBotCommandButton10:LeftButton"] = "Unbot Command ActionButton 10";
+	_G["BINDING_NAME_CLICK UnBotCommandButtonFunc:LeftButton"] = "Unbot Close Frame ActionButton";
 end
